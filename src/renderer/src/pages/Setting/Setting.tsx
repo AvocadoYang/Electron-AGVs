5a551fc68d71328cc93666cd5281fb466239a2a4
charlie <charlie@ubuntu20.04>
2025-02-05 19:07:11 +0800

    Feat: 新增表單拖曳

--- src/renderer/src/pages/Setting/Setting.tsx
+++ src/renderer/src/pages/Setting/Setting.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React, { useState, useRef, useEffect } from 'react'
-import { Layout, Form, Flex, Splitter, Typography, Input } from 'antd'
+import { Layout, Form, Splitter, FormInstance } from 'antd'
 import Header from '../../components/Header'
 import { ZoomPad, Sider, FormDrawerBtn } from './components'
 import { useAtom } from 'jotai'
@@ -11,7 +11,59 @@ import { AllLocationTable } from './formComponent/forms'
 import MapView from './mapComponents/MapView'
 import { useResetSiderSwitch } from './hooks'
 import './setting.css'
+import {CSS} from '@dnd-kit/utilities'
+import { DndContext } from '@dnd-kit/core'
+import { arrayMove, SortableContext, verticalListSortingStrategy, useSortable } from '@dnd-kit/sortable'
+import { restrictToParentElement } from '@dnd-kit/modifiers'
 const { Content } = Layout
+
+const getMoveIndex = (array, dragItem) => {
+  const { active, over } = dragItem;
+  let activeIndex = 0;
+  let overIndex = 0;
+  try {
+      // 遍历数组，查找出active和over的index
+      array.forEach((item, index) => {
+          if (active.id === item.key) {
+              activeIndex = index;
+          }
+          if (over.id === item.key) {
+              overIndex = index;
+          }
+      });
+  } catch (error) {
+      overIndex = activeIndex; // 如果有问题，则复位
+  }
+  return { activeIndex, overIndex };
+};
+
+ // 拖拽项组件
+ const SortableItem: React.FC<{ locationPanelForm: FormInstance<unknown>, itemProps:{
+  key: string;
+  width: number;
+  isChecked: boolean;
+  title: string;
+}}> = ({itemProps: checkboxItem, locationPanelForm}) => {
+  // 父传子，从props里拿，建议使用其他名字（如itemProps）代替props，以免和父组件的props混淆
+  const { setNodeRef, attributes, listeners, transform, transition } = useSortable({
+      id: checkboxItem.key, // 这里传入的id属性必须和SortableContext的items数组一一对应
+      transition: {
+          duration: 500,
+          easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
+      },
+  });
+  const styles = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+  };
+
+  return (
+        <div ref={setNodeRef} {...attributes} style={styles}>
+        <EditLocationPanel locationPanelForm={locationPanelForm} listeners={listeners}></EditLocationPanel>
+      </div>
+
+  );
+};
 
 
 
@@ -24,6 +76,13 @@ const Setting: React.FC = () => {
   const mapWrapRef = useRef(null)
   const [locationPanelForm] = Form.useForm()
   const [roadPanelForm] = Form.useForm()
+  const [dataList, setDataList] = useState([
+    { key: 'name', width: 0.25, isChecked: true, title: '姓名' },
+    { key: 'age', width: 0.25, isChecked: true, title: '年龄' },
+    { key: 'sex', width: 0.25, isChecked: true, title: '性别' },
+    { key: 'phone', width: 0.25, isChecked: true, title: '手机号' },
+]);
+
 
   const [scale, setScale] = useState(1)
 
@@ -39,6 +98,19 @@ const Setting: React.FC = () => {
     }))
     setTempStoredLocation([...storedData])
   }, [data])
+
+
+
+  const dragEndEvent = (dragItem) => {
+    setDataList((prevDataList) => {
+        const moveDataList = [...prevDataList];
+        const { activeIndex, overIndex } = getMoveIndex(moveDataList, dragItem);
+        const newDataList = arrayMove(moveDataList, activeIndex, overIndex);
+        return newDataList;
+    });
+};
+
+
 
   useResetSiderSwitch()
 
@@ -58,6 +130,19 @@ const Setting: React.FC = () => {
             >
               <Splitter>
                 <Splitter.Panel defaultSize="40%">
+                <DndContext onDragEnd={dragEndEvent} modifiers={[restrictToParentElement]}>
+                  <SortableContext items={dataList.map((c) => c.key)} strategy={verticalListSortingStrategy}>
+                      {/* 这里的items接收一个数组，这个数组的值要和useSortable传入的id属性一一对应 */}
+                      <div className="attrs">
+
+                              {dataList.map((checkboxItem) => (
+                                  <SortableItem itemProps={checkboxItem} key={checkboxItem.key} locationPanelForm={locationPanelForm} />
+                              ))}
+
+                      </div>
+                  </SortableContext>
+              </DndContext>
+
                 </Splitter.Panel>
                 <Splitter.Panel defaultSize="60%" min="20%">
                   <MapView
@@ -80,7 +165,7 @@ const Setting: React.FC = () => {
 
         {
           /** 1-1 編輯點位的彈跳視窗 */
-          <EditLocationPanel locationPanelForm={locationPanelForm}></EditLocationPanel>
+          // <EditLocationPanel locationPanelForm={locationPanelForm}></EditLocationPanel>
         }
         {
           /** 1-4 顯示地點列表 */
--- src/renderer/src/pages/Setting/formComponent/forms/EditLocationPanel.tsx
+++ src/renderer/src/pages/Setting/formComponent/forms/EditLocationPanel.tsx
@@ -2,13 +2,12 @@
 import React, { memo } from 'react'
 import { LocationType } from '@renderer/utils/jotai'
 import { useAtom } from 'jotai'
+import './form.css'
 import { EditLocationPanelSwitch } from '@renderer/utils/siderGloble'
 import {  tempStoredLocation } from '@renderer/utils/gloable'
-import DraggableWindow from '../DraggableWindow'
 import { openNotificationWithIcon } from '../../utils/notification'
 import { useTranslation } from 'react-i18next'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { CloseOutlined } from '@ant-design/icons'
 import { Form, Input, Radio, Button, FormInstance, Checkbox, message, Card } from 'antd'
 import { initialLocationFormValue } from './formInitValue'
 import client  from '@renderer/api/axiosClient'
@@ -17,11 +16,12 @@ import { errorHandler } from '@renderer/utils/utils'
 
 const EditLocationPanel: React.FC<{
   locationPanelForm: FormInstance<unknown>
-}> = ({ locationPanelForm }) => {
+  listeners: import("@dnd-kit/core/dist/hooks/utilities").SyntheticListenerMap | undefined;
+}> = ({ locationPanelForm, listeners }) => {
   const [openEditLocationPanel, setOpenEditLocationPanel] = useAtom(EditLocationPanelSwitch)
   const [TempStoredLocation] = useAtom(tempStoredLocation)
   const queryClient = useQueryClient();
-  const [messageApi, contextHolders] = message.useMessage();
+  const [messageApi, contextHolders] = message.useMessage()
   const { t } = useTranslation()
 
   const saveLocationMutation = useMutation({
@@ -88,151 +88,72 @@ const EditLocationPanel: React.FC<{
   return (
     <>
       {openEditLocationPanel && (
-        <DraggableWindow width="15%" isHide={false}>
+        <>
           {contextHolders}
-          <Card
-            title={t('toolbar.location.edit_locations')}
-            style={{
-              width: '100%',
-              maxWidth: 400,
-              padding: '16px',
-              boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-            }}
-          >
-      <Form
-        layout="vertical"
-        initialValues={initialLocationFormValue}
-        form={locationPanelForm}
-        style={{ maxWidth: '100%' }}
-      >
-        <Form.Item label="X" name="x" style={{ marginBottom: 16 }} required>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Y" name="y" style={{ marginBottom: 16 }} required>
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="θ"
-          name="rotation"
-          style={{ marginBottom: 16 }}
-          rules={[
-            { required: true, message: '必填' },
-            { max: 360, message: '不可超過360' },
-            { min: -360, message: '不可小於-360' },
-          ]}
-        >
-          <Input type="number" />
-        </Form.Item>
-
-        <Form.Item
-         label="是否可旋轉" name="canRotate" valuePropName="checked" shouldUpdate
-          style={{ marginBottom: 16 }}
-        >
-          <Checkbox />
-        </Form.Item>
-
-        <Form.Item
-          label="ID"
-          name="locationId"
-          style={{ marginBottom: 16 }}
-          rules={[{ required: true, message: '必填' }]}
-        >
-          <Input type="number" />
-        </Form.Item>
-
-        <Form.Item
-          label={"功能"}
-          name="areaType"
-          style={{ marginBottom: 16 }}
-        >
-          <Radio.Group>
-          <Radio value="Extra">{t('edit_location_panel.none')}</Radio>
-                <Radio value="充電區">{t('edit_location_panel.charge_station')}</Radio>
-                <Radio value="預派點">{t('edit_location_panel.prepare_spot')}</Radio>
-                <Radio value="預派點">{t('edit_location_panel.wait_side')}</Radio>
-                <Radio value="存貨區">{t('edit_location_panel.shelve')}</Radio>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item style={{ textAlign: 'center' }}>
-              <Button onClick={savePose} type="primary">
-                {t('edit_location_panel.save')}
-              </Button>
-            </Form.Item>
-      </Form>
-    </Card>
-          {/* <div
-            style={{
-              display: 'flex',
-              justifyContent: 'end',
-              alignItems: 'center',
-              width: '100%'
-            }}
-          >
-            <CloseOutlined
-              onClick={() => {
-                setOpenEditLocationPanel(false)
-              }}
-            />
-          </div> */}
-          {/* <Form
-            initialValues={initialLocationFormValue}
-            form={locationPanelForm}
-            style={{ paddingTop: '15px' }}
-          >
-            <Form.Item label="X" name="x" shouldUpdate required>
-              <Input />
-            </Form.Item>
-
-            <Form.Item label="Y" name="y" shouldUpdate required>
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="θ"
-              name="rotation"
-              shouldUpdate
-              hasFeedback
-              validateDebounce={1000}
-              rules={[
-                { required: true, message: '必填' },
-                { max: 360, message: '不可超過360' },
-                { min: -360, message: '不可小於-360' }
-              ]}
+          <div className='edit_location_panel_wrap' {...listeners}>
+            <Form
+              layout="vertical"
+              initialValues={initialLocationFormValue}
+              form={locationPanelForm}
             >
-              <Input type="number" />
-            </Form.Item>
+              <Form.Item label="X" name="x" style={{ marginBottom: 16 }} required>
+                <Input />
+              </Form.Item>
 
-            <Form.Item label="是否可旋轉" name="canRotate" valuePropName="checked" shouldUpdate>
-              <Checkbox />
-            </Form.Item>
+              <Form.Item label="Y" name="y" style={{ marginBottom: 16 }} required>
+                <Input />
+              </Form.Item>
 
-            <Form.Item
-              label="ID"
-              name="locationId"
-              shouldUpdate
-              rules={[{ required: true, message: '必填' }]}
-            >
-              <Input type="number" />
-            </Form.Item>
-            <Form.Item label="功能" name="areaType" shouldUpdate>
-              <Radio.Group value="Extra">
+              <Form.Item
+                label="θ"
+                name="rotation"
+                style={{ marginBottom: 16 }}
+                rules={[
+                  { required: true, message: '必填' },
+                  { max: 360, message: '不可超過360' },
+                  { min: -360, message: '不可小於-360' },
+                ]}
+              >
+                <Input type="number" />
+              </Form.Item>
+
+              <Form.Item
+              label="是否可旋轉" name="canRotate" valuePropName="checked" shouldUpdate
+                style={{ marginBottom: 16 }}
+              >
+                <Checkbox />
+              </Form.Item>
+
+              <Form.Item
+                label="ID"
+                name="locationId"
+                style={{ marginBottom: 16 }}
+                rules={[{ required: true, message: '必填' }]}
+              >
+                <Input type="number" />
+              </Form.Item>
+
+              <Form.Item
+                label={"功能"}
+                name="areaType"
+                style={{ marginBottom: 16 }}
+              >
+                <Radio.Group>
                 <Radio value="Extra">{t('edit_location_panel.none')}</Radio>
-                <Radio value="充電區">{t('edit_location_panel.charge_station')}</Radio>
-                <Radio value="預派點">{t('edit_location_panel.prepare_spot')}</Radio>
-                <Radio value="預派點">{t('edit_location_panel.wait_side')}</Radio>
-                <Radio value="存貨區">{t('edit_location_panel.shelve')}</Radio>
-              </Radio.Group>
-            </Form.Item>
-
-            <Form.Item style={{ textAlign: 'center' }}>
-              <Button onClick={savePose} type="primary">
-                {t('edit_location_panel.save')}
-              </Button>
-            </Form.Item>
-          </Form> */}
-        </DraggableWindow>
+                      <Radio value="充電區">{t('edit_location_panel.charge_station')}</Radio>
+                      <Radio value="預派點">{t('edit_location_panel.prepare_spot')}</Radio>
+                      <Radio value="待命區">{t('edit_location_panel.wait_side')}</Radio>
+                      <Radio value="存貨區">{t('edit_location_panel.shelve')}</Radio>
+                </Radio.Group>
+              </Form.Item>
+              <Form.Item style={{ textAlign: 'center' }}>
+                    <Button onClick={savePose} type="primary">
+                      {t('edit_location_panel.save')}
+                    </Button>
+                  </Form.Item>
+            </Form>
+            </div>
+        </>
       )}
     </>
   )
