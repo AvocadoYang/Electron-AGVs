5e3889817b421a32716cb221fff182cf443d9185
charlie <charlie@ubuntu20.04>
2025-03-31 15:47:35 +0800

    Style: add cube icon

--- src/renderer/src/pages/Main/components/WebView/components/MissionBtn.tsx
+++ src/renderer/src/pages/Main/components/WebView/components/MissionBtn.tsx
@@ -1,4 +1,4 @@
-import {
+import Icon, {
   ThunderboltOutlined,
   RedoOutlined,
   CalendarOutlined,
@@ -6,19 +6,31 @@ import {
   PauseOutlined
 } from '@ant-design/icons';
 import '../webview.css';
-import { Button, Flex } from 'antd';
+import { Button, Flex, GetProps } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { DialogMission } from '../../missionModal';
 import { memo, useState } from 'react';
 import { OpenAssignMission } from '@renderer/pages/Main/global/jotai';
 import { useSetAtom } from 'jotai';
 import QuickMissionWebView from '../../missionModal/QuickMissionWebView';
+type CustomIconComponentProps = GetProps<typeof Icon>;
+
+const Cube = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <title>cube-outline</title>
+    <path d="M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L6.04,7.5L12,10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V9.21L13,12.58V19.29L19,15.91Z" />
+  </svg>
+);
 
 const MissionBtn = () => {
   const { t } = useTranslation();
   const openAssignMission = useSetAtom(OpenAssignMission);
   const [showQuickMission, setShowQuickMission] = useState(false);
   const [isStart, setIsStart] = useState(true);
+
+  const CubeIcon = (props: Partial<CustomIconComponentProps>) => (
+    <Icon component={Cube} {...props} />
+  );
   return (
     <>
       <Flex gap="small" wrap="wrap" align="center" justify="end" className="mission-btn-wrap">
@@ -62,6 +74,7 @@ const MissionBtn = () => {
           className={`${isStart ? 'pause-button' : 'hide-pause'}`}
           onClick={() => setIsStart(false)}
         />
+        <CubeIcon style={{ fontSize: '100px' }}></CubeIcon>
       </Flex> */}
     </>
   );
