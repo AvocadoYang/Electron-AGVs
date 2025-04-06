73729611e1d2cb380df73c7c734cfd9f98a955ee
charlie <charlie@ubuntu20.04>
2025-04-06 15:17:14 +0800

    Style: 佔領顏色更改

--- src/renderer/src/pages/Setting/mapComponents/components/AllRoads/Road.tsx
+++ src/renderer/src/pages/Setting/mapComponents/components/AllRoads/Road.tsx
@@ -1,44 +1,44 @@
- 
-
-import { FC, useRef } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { useAtomValue } from 'jotai';
 import { Tooltip } from 'antd';
 import { rad2Deg } from '@renderer/utils/utils';
 import { isShowRoadTooltip } from '@renderer/utils/siderGloble';
+import useMockRobot from '@renderer/api/useMockRobot';
 
 const Container = styled.div.attrs<{
-  left: number
-  top: number
+  left: number;
+  top: number;
 }>(({ left, top }) => ({ style: { left, top } }))<{
-  left: number
-  top: number
+  left: number;
+  top: number;
 }>`
   position: absolute;
 `;
 
 const Line = styled.div.attrs<{
-  length: number
-  angle: number
-  $isOneWayRoad: boolean
-  $isClaimed: boolean
-  $limit: boolean
-  $isOnHover: boolean
-}>(({ length, angle, $isOneWayRoad, $isClaimed, $isOnHover }) => ({
+  length: number;
+  angle: number;
+  color: string;
+  $isOneWayRoad: boolean;
+  $isClaimed: boolean;
+  $limit: boolean;
+  $isOnHover: boolean;
+}>(({ length, angle, $isOneWayRoad, $isClaimed, $isOnHover, color }) => ({
   style: {
     width: length,
     height: $isOnHover ? '2px' : '1px',
     transform: `rotate(${angle}deg) translateY(-50%)`,
     backgroundColor: $isOneWayRoad ? '#ffffff5' : 'rgb(0 68 255 / 0%)',
-    border: $isClaimed || $isOnHover ? '1px solid #ff9646' : '2px solid #cccccc47'
+    border: $isClaimed || $isOnHover ? `1px solid ${color}` : '2px solid #cccccc47'
   }
 }))<{
-  length: number
-  angle: number
-  $isOneWayRoad: boolean
-  $isClaimed: boolean
-  $limit: boolean
-  $isOnHover: boolean
+  length: number;
+  angle: number;
+  $isOneWayRoad: boolean;
+  $isClaimed: boolean;
+  $limit: boolean;
+  $isOnHover: boolean;
 }>`
   height: 4px;
   transform-origin: top left;
@@ -72,23 +72,34 @@ const Line = styled.div.attrs<{
 `;
 
 const Road: FC<{
-  roadId: string
-  roadType: 'oneWayRoad' | 'twoWayRoad'
-  x1: number // in pixel, css coordinate
-  y1: number // in pixel, css coordinate
-  x2: number // in pixel, css coordinate
-  y2: number // in pixel, css coordinate
-  validYawList: '*' | number[]
-  disabled: boolean
-  isClaimedBy?: string
-  limit: boolean
-  isRoadOnHover: boolean
+  roadId: string;
+  roadType: 'oneWayRoad' | 'twoWayRoad';
+  x1: number; // in pixel, css coordinate
+  y1: number; // in pixel, css coordinate
+  x2: number; // in pixel, css coordinate
+  y2: number; // in pixel, css coordinate
+  validYawList: '*' | number[];
+  disabled: boolean;
+  isClaimedBy?: string;
+  limit: boolean;
+  isRoadOnHover: boolean;
 }> = ({ roadId, roadType, x1, y1, x2, y2, isClaimedBy, limit, disabled, isRoadOnHover }) => {
   const ref = useRef(null);
+  const [simulateColor, setSimulateColor] = useState('#ff9646');
   const length = Math.hypot(x1 - x2, y1 - y2);
   const angle = rad2Deg(Math.atan2(y2 - y1, x2 - x1));
+  const { data: script } = useMockRobot();
 
   const showRoadTooltip = useAtomValue(isShowRoadTooltip);
+
+  useEffect(() => {
+    if (!script) return;
+    if (script.isSimulate) {
+      setSimulateColor('#7ca1ea');
+    } else {
+      setSimulateColor('#ff9646');
+    }
+  }, [script]);
 
   return (
     <Container left={x1} top={y1}>
@@ -97,6 +108,7 @@ const Road: FC<{
           length={length}
           angle={angle}
           ref={ref}
+          color={simulateColor}
           $isOneWayRoad={roadType === 'oneWayRoad'}
           $isClaimed={isClaimedBy !== undefined}
           $limit={!!limit}
